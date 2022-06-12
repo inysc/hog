a3243d8c5729623c2f9012a5f3b65c017ee966b4
SunspotsInys <inysuay@gmail.com>
2022-06-12 12:05:29 +0800

    :new: add multi writer

--- log.go
+++ log.go
@@ -77,3 +77,59 @@ func New(name string, lvl Level, w io.Writer) (l *Logger) {
 		err:    name + "[ERROR][",
 	}
 }
+
+func NewWriter(ws ...io.Writer) io.Writer {
+	switch len(ws) {
+	case 0:
+		return os.Stdout
+	case 1:
+		return ws[0]
+	default:
+		mw := MultiWriter{}
+		mw = append(mw, ws...)
+		return mw
+	}
+}
+
+type MultiWriter []io.Writer
+
+type errs []error
+
+func (err *errs) Error() string {
+	switch len(*err) {
+	case 0:
+		return "<nil>"
+	case 1:
+		return (*err)[0].Error()
+	default:
+		bs := bpl.Get().(*buf)
+		bs.buf = bs.buf[:0]
+		bs.buf = append(bs.buf, []byte("multi error\n")...)
+		for _, v := range *err {
+			bs.buf = append(bs.buf, []byte(" - ")...)
+			bs.buf = append(bs.buf, []byte(v.Error())...)
+			bs.buf = append(bs.buf, '\n')
+		}
+		ret := string(bs.buf)
+		bpl.Put(bs)
+		return ret
+	}
+}
+
+func (err *errs) Append(e error) {
+	if err == nil {
+		err = &errs{}
+	}
+	if e != nil {
+		*err = append(*err, e)
+	}
+}
+
+func (mw MultiWriter) Write(p []byte) (n int, err error) {
+	var me *errs
+	for i := range mw {
+		n, err = mw[i].Write(p)
+		me.Append(err)
+	}
+	return n, me
+}
